3200b2a1346a0bc8f094457e9ace927e54f8e603
manucho.dev <mbmanueldev@gmail.com>
2024-12-12 19:22:24 -0300

    minor change 2

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,5 +1,5 @@
 const ENVIROMENT = {
-'URL_BACKEND': 'https://utn-pwa-full-stack-back-end-deploy.vercel.app/api'
+'URL_BACKEND': 'https://utn-pwa-full-stack-back-end-deploy.vercel.app'
 }
 
 export default ENVIROMENT
