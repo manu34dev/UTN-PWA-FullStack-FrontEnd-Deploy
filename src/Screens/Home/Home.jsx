1d275f0b158334390ce493da78476aee360f3083
manucho.dev <mbmanueldev@gmail.com>
2024-12-16 05:51:14 -0300

    collapse fixed

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -15,8 +15,7 @@ const Home = () => {
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
                     <a class="navbar-brand" href="#!">Bienvenido {user_info.name}</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div>
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href='/product/new'>Registra tu producto</a></li>
                         </ul>
